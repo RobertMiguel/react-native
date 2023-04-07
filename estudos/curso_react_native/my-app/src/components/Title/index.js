03127147ae1504f92b52cc8d1e7e7a2974e88860
RobertMiguel <robertresende9@gmail.com>
2023-04-07 12:45:44 -0300

    Projetos para Estudos

---
+++ estudos/curso_react_native/my-app/src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function Title() {
+    return (
+        <View>
+            <Text>Academia das Maravilhas</Text>
+        </View>
+    )
+}
